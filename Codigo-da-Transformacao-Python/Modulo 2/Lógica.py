79ffd6de9798006f4ff300ef69e5f535bd2ca4fb
Renan em Vocacao <renangcoelho.15@gmail.com>
2025-07-20 21:32:25 -0300

    Update Lógica.py

--- Codigo-da-Transformacao-Python/Modulo 2/Lógica.py
+++ Codigo-da-Transformacao-Python/Modulo 2/Lógica.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+# Lógica: Verifica se o número digitado é positivo, negativo ou zero
+
+numero = int(input("Digite um número: "))
+
+if numero > 0:
+    print("O número é positivo.")
+elif numero < 0:
+    print("O número é negativo.")
+else:
+    print("O número é zero.")
